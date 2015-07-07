f08dfc3ddc4e3419282f43f9883dd9f8b7bf0fec
Chandrakant Gopalan <chandrakant.gopalan@gmail.com>
2015-07-07 13:24:08 -0400

    Changed cust vars

--- pyrunprocs/client_mongoexup.py
+++ pyrunprocs/client_mongoexup.py
@@ -1,15 +1,11 @@
 import mongoexup as meu
 
 
-def profile_pre_hook():
+def cust_post_hook():
     pass
 
 
-def profile_post_hook():
-    pass
-
-
-profile_fields = (
+cust_fields = (
             "_id,signup_time,optout_time,first_purchase_time,"
             "purchase_time,purchase_count,purchase_price,horizon_count,"
             "lifetime_click,click_time,lifetime_open,open_time,"
@@ -25,29 +21,20 @@ profile_fields = (
         )
 
 
-def profile_pre_hook(client_id, is_partitioned, is_sharded, profile_rs):
-    SAILTHRU_DB = "sailthru"
-    if is_partitioned:
-        query = "{}"
-        collection = "profile.{0}".format(client_id)
-        db_name = SAILTHRU_DB
-    elif is_sharded:
-        query = "{client_id : " + str(client_id) + "}"
-        collection = "profile"
-        db_name = profile_rs
-    else:
-        query = "{client_id : " + str(client_id) + "}"
-        collection = "profile"
-        db_name = SAILTHRU_DB
+def cust_pre_hook(client_id, is_partitioned, is_sharded, profile_rs):
+    TEST_DB = "testdb"
+    query = "{}"
+    collection = "cust.{0}".format(client_id)
+    db_name = TEST_DB
     return query, collection, db_name
 
 
 task_list = [
                 {'operation': meu.EX_EXPORT,
-                 'args': ['--host', 'localhost:27017', '--db', 'profileC', '--collection', 'profile',
-                        '--fields', profile_fields, '--query', '{client_id: 4320}', '--out', 'profile.json'],
-                 'pre_hook' : profile_pre_hook,
-                 'post_hook': profile_post_hook},
+                 'args': ['--host', 'localhost:27017', '--db', 'testdb', '--collection', 'cust',
+                        '--fields', cust_fields, '--query', '{client_id: 1234}', '--out', 'cust.json'],
+                 'pre_hook' : cust_pre_hook,
+                 'post_hook': cust_post_hook},
 
             ]
 
@@ -55,6 +42,6 @@ def test_mongoexup():
     meu.run(task_list)
 
 if __name__ == '__main__':
-    print("Running mongoexup")
+    print("Running task")
     test_mongoexup()
 
